f5c048900dc5194c6a930d08e6ecaaba6fc6e2d0
Nevilstein <keikirishima891@gmail.com>
2019-03-06 18:46:18 +0800

    update1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NovelsComponent } from './novels/novels.component';
+import { HomeComponent } from './home/home.component';
+import { WomenComponent } from './women/women.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovelsComponent,
+    HomeComponent,
+    WomenComponent
   ],
   imports: [
     BrowserModule,
